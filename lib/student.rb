5c8467161c11c19a44b52f9b43333831d15e4ebd
Harley McDowell <hmcdowell95@gmail.com>
2020-03-29 22:44:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT name FROM students WHERE grade < 12
+    SELECT name FROM students WHERE grade = 10
     SQL
     DB[:conn].execute(sql)
   end
