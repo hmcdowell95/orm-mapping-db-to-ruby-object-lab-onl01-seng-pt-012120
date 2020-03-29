6662305d255ebf8df752fad718ef0495c5264eb2
Harley McDowell <hmcdowell95@gmail.com>
2020-03-29 23:44:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,13 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
-    
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    SQL
+    a = x - 1
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end[0..a]
   end
   
   def self.first_student_in_grade_10
